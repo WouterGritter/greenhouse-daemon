514e111b5ae8a2907c7c4723a6bf677ad7e109d2
Wouter Gritter <wouter@gritter.me>
2024-05-03 20:04:51 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# greenhouse-daemon
+A little script to change the color of a ledstrip placed in a greenhouse based on a temperature sensor placed in the same greenhouse.
---
+++ main.py
@@ -0,0 +1,76 @@
+import os
+import time
+
+import requests
+import tinytuya
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TEMP_SENSOR_URL = os.getenv('TEMP_SENSOR_URL')
+TUYA_DEVICE_ID = os.getenv('TUYA_DEVICE_ID')
+TUYA_LOCAL_KEY = os.getenv('TUYA_LOCAL_KEY')
+TUYA_ADDRESS = os.getenv('TUYA_ADDRESS')
+MIN_TEMPERATURE = float(os.getenv('MIN_TEMPERATURE', 10))
+MAX_TEMPERATURE = float(os.getenv('MAX_TEMPERATURE', 50))
+COLD_COLOR = tuple(float(x) for x in os.getenv('COLD_COLOR', '0,0,255').split(','))
+MID_COLOR = tuple(float(x) for x in os.getenv('MID_COLOR', '255,204,0').split(','))
+HOT_COLOR = tuple(float(x) for x in os.getenv('HOT_COLOR', '255,0,0').split(','))
+UPDATE_INTERVAL = float(os.getenv('UPDATE_INTERVAL', 60))
+
+print(f'{TEMP_SENSOR_URL=}')
+print(f'{TUYA_DEVICE_ID=}')
+print(f'{TUYA_LOCAL_KEY=}')
+print(f'{TUYA_ADDRESS=}')
+print(f'{MIN_TEMPERATURE=}')
+print(f'{MAX_TEMPERATURE=}')
+print(f'{COLD_COLOR=}')
+print(f'{MID_COLOR=}')
+print(f'{HOT_COLOR=}')
+print(f'{UPDATE_INTERVAL=}')
+
+
+def scale_color(value, low_color, high_color):
+    return (value * (high_color[0] - low_color[0]) + low_color[0],
+            value * (high_color[1] - low_color[1]) + low_color[1],
+            value * (high_color[2] - low_color[2]) + low_color[2])
+
+
+def generate_color(temp):
+    t_scaled = (temp - MIN_TEMPERATURE) / (MAX_TEMPERATURE - MIN_TEMPERATURE)
+    if t_scaled < 0.5:
+        return scale_color(t_scaled * 2, COLD_COLOR, MID_COLOR)
+    else:
+        return scale_color((t_scaled - 0.5) * 2, MID_COLOR, HOT_COLOR)
+
+
+def fetch_temperature():
+    data = requests.get(TEMP_SENSOR_URL).json()
+    return data['temperature']
+
+
+def main():
+    led_strip = tinytuya.BulbDevice(
+        dev_id=TUYA_DEVICE_ID,
+        address=TUYA_ADDRESS,
+        local_key=TUYA_LOCAL_KEY,
+        version=3.3
+    )
+
+    status = led_strip.status()
+    print(f'Connected to led strip at {TUYA_ADDRESS}. Status: ${status}')
+
+    print(f'Updating led strip color according to temperature every {UPDATE_INTERVAL} seconds.')
+    while True:
+        try:
+            temp = fetch_temperature()
+            r, g, b = generate_color(temp)
+            led_strip.set_colour(r, g, b)
+        except Exception as e:
+            print(f'Error performing update! {e}')
+
+        time.sleep(UPDATE_INTERVAL)
+
+
+if __name__ == '__main__':
+    main()
