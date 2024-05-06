c7176d7b6651eacd291b889cd21ee9c369f167a9
Wouter Gritter <wouter@gritter.me>
2024-05-06 22:31:56 +0200

    Add support to turn the ledstrip off between certain times

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import os
 import time
+from datetime import datetime
+from typing import Optional
 
 import requests
 import tinytuya
 from dotenv import load_dotenv
 
 load_dotenv()
+
+
+def parse_time_safe(time_str: str, format_str: str = '%H:%M') -> Optional[time]:
+    try:
+        return datetime.strptime(time_str, format_str).time()
+    except ValueError:
+        return None
+
 
 TEMP_SENSOR_URL = os.getenv('TEMP_SENSOR_URL')
 TUYA_DEVICE_ID = os.getenv('TUYA_DEVICE_ID')
@@ -17,6 +27,8 @@ COLD_COLOR = tuple(float(x) for x in os.getenv('COLD_COLOR', '0,0,255').split(',
 MID_COLOR = tuple(float(x) for x in os.getenv('MID_COLOR', '255,204,0').split(','))
 HOT_COLOR = tuple(float(x) for x in os.getenv('HOT_COLOR', '255,0,0').split(','))
 UPDATE_INTERVAL = float(os.getenv('UPDATE_INTERVAL', 60))
+OFF_TIME = parse_time_safe(os.getenv('OFF_TIME', '00:00'))
+ON_TIME = parse_time_safe(os.getenv('ON_TIME', '06:00'))
 
 print(f'{TEMP_SENSOR_URL=}')
 print(f'{TUYA_DEVICE_ID=}')
@@ -28,6 +40,8 @@ print(f'{COLD_COLOR=}')
 print(f'{MID_COLOR=}')
 print(f'{HOT_COLOR=}')
 print(f'{UPDATE_INTERVAL=}')
+print(f'{OFF_TIME=}')
+print(f'{ON_TIME=}')
 
 
 def clamp(value, min_value, max_value):
@@ -67,14 +81,39 @@ def build_led_strip():
     )
 
 
+def time_in_range(start: time, end: time, x: Optional[time] = None):
+    if x is None:
+        x = datetime.now().time()
+
+    if start <= end:
+        return start <= x <= end
+    else:
+        return start <= x or x <= end
+
+
 def main():
     led_strip = build_led_strip()
 
     status = led_strip.status()
     print(f'Connected to led strip at {TUYA_ADDRESS}. Status: ${status}')
 
+    is_on = False
+
     print(f'Updating led strip color according to temperature every {UPDATE_INTERVAL} seconds.')
     while True:
+        if ON_TIME is not None and OFF_TIME is not None and time_in_range(OFF_TIME, ON_TIME):
+            if is_on:
+                print('Turning led strip OFF.')
+                led_strip.turn_off()
+                is_on = False
+
+            continue
+
+        if not is_on:
+            print('Turning led strip ON.')
+            led_strip.turn_on()
+            is_on = True
+
         try:
             status = led_strip.status()
             if status is None or status['dps'] is None or status['dps']['21'] is None:
